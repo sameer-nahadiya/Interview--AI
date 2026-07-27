81c6fd3d61fbeb4f7398afffdfa396fea6bda306
sameer-nahadiya <samnahadiya211@gmail.com>
2026-07-28 00:25:40 +0530

    styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,8 @@
-
+import {RouterProvider} from "react-router-dom"
+import {router} from "./app.route.jsx"
 function App() {
-
-
   return (
-    <>  </> 
+    <RouterProvider router={router} />
   )
 }
 
--- Frontend/src/app.route.jsx
+++ Frontend/src/app.route.jsx
@@ -1,1 +1,19 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {createBrowserRouter, Navigate} from "react-router-dom"
+import Login from "./features/auth/pages/login"
+import Register from "./features/auth/pages/Register"
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/",
+        element: <Navigate to="/login" replace />
+    }
+])
